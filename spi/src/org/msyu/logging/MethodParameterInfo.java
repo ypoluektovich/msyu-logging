7661710a61c3844409d9c8ad7f9cc99767442c7e
Yanus Poluektovich <ypoluektovich@gmail.com>
2014-11-23 01:26:27 +0300

    add equals() and hashCode() to MethodParameterInfo

--- spi/src/org/msyu/logging/MethodParameterInfo.java
+++ spi/src/org/msyu/logging/MethodParameterInfo.java
@@ -15,4 +15,22 @@ public final class MethodParameterInfo {
 		return Throwable.class.isAssignableFrom(type) && argument != null;
 	}
 
+	@Override
+	public final boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null || getClass() != obj.getClass()) {
+			return false;
+		}
+		MethodParameterInfo that = (MethodParameterInfo) obj;
+		return name.equals(that.name) && type.equals(that.type);
+
+	}
+
+	@Override
+	public final int hashCode() {
+		return 31 * name.hashCode() + type.hashCode();
+	}
+
 }
